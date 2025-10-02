bea8ee74e1481ebb64008bdf2993b1b1e841d9eb
Gerwin15 <gerwin_cando@rcc.edu.ph>
2025-10-02 11:26:17 +0800

    Added Subtitle

--- mainsystem/src/components/attendance/MainAttendanceScreen.tsx
+++ mainsystem/src/components/attendance/MainAttendanceScreen.tsx
@@ -124,8 +124,9 @@ export const MainAttendanceScreen = () => {
           <h1 className="text-4xl md:text-5xl font-bold text-white mb-2">
             REPUBLIC CENTRAL COLLEGES
           </h1>
-          <p className="text-lg md:text-3xl text-white font-semibold tracking-wide">
-            Time and Record Attendance Checking System (RCC-TRACS)
+          <p className="text-lg md:text-4xl text-white font-semibold tracking-wide">
+            Time and Record Attendance Checking System (RCC-TRACS): <br />
+            A Web-Based Solution with Data Analytics for Enhanced Monitoring
           </p>
           <p className="mt-4 text-2xl md:text-3xl text-yellow-200 font-bold">
             {currentTime}
