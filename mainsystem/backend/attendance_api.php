2979e893e66f1856c2d1def530787d1278a645b8
Gerwin15 <gerwin_cando@rcc.edu.ph>
2026-07-07 08:20:37 +0800

    Updated the image source for email service

--- mainsystem/backend/attendance_api.php
+++ mainsystem/backend/attendance_api.php
@@ -309,7 +309,7 @@ if ($method === 'POST') {
                     color: #222;
                     text-align: center;
                   ">
-                    <img src="https://scontent.fcrk1-5.fna.fbcdn.net/v/t39.30808-6/598618782_122279774894198393_5794352533800714966_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_ohc=c-Rk3Uqx_qEQ7kNvwH4uS-I&_nc_oc=AdlXW3j7M7OhIbQ4kJ9z3gfaDJyLb9DQrgoqV8mzfZ-3qw1A7yVErwzbEIu4zLIzvAI&_nc_zt=23&_nc_ht=scontent.fcrk1-5.fna&_nc_gid=UcHjZdacJ8ZJODy0FR3ygA&oh=00_AfnDFNcL3Rw-dI9tKMuCPIzGYe4-TgeBZhzvdYfIG43B9g&oe=6941347C" alt="TRAC System" style="width:100px;height:100px;object-fit:contain;margin-bottom:18px;" />
+                   <img src="https://i.ibb.co/1fx7h5R0/trac-seal.png" alt="RCC TRACS logo" border="0"  style="width:100px;height:100px;object-fit:contain;margin-bottom:18px;" />
                     <h2 style="font-size:1.6rem;font-weight:700;margin-bottom:18px;">Attendance Notification</h2>
                     <p style="font-size:1.1rem;margin-bottom:18px;">
                       Your child, <b>' . htmlspecialchars($studentName) . '</b> has entered RCC at:
